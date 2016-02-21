c5b12b150476aaa2875a1fa520b8c376f291c297
Hirofumi Wakasugi <baenej@gmail.com>
2016-02-21 20:39:32 +0900

    wip

--- Gemfile
+++ Gemfile
@@ -18,8 +18,8 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+# # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+# gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # Action Cable dependencies for the Redis adapter
@@ -44,3 +44,8 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+gem 'webpack-rails'
+gem 'slim-rails'
+gem 'foreman'
---
+++ config/webpack.config.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var StatsPlugin = require('stats-webpack-plugin');
+
+// must match config.webpack.dev_server.port
+var devServerPort = 3808;
+
+// set TARGET=production on the environment to add asset fingerprints
+var production = process.env.TARGET === 'production';
+
+var config = {
+  entry: {
+    'application': path.join(__dirname, '..', 'app/assets/javascripts/application.js')
+  },
+
+  output: {
+    // Build assets directly in to public/webpack/, let webpack know
+    // that all webpacked assets start with webpack/
+
+    // must match config.webpack.output_dir
+    path: path.join(__dirname, '..', 'public', 'webpack'),
+    publicPath: '/webpack/',
+
+    filename: production ? '[name]-[chunkhash].js' : '[name].js'
+  },
+
+  resolve: {
+    root: path.join(__dirname, '..', 'app/assets/javascripts'),
+    modulesDirectories: [
+      path.join(__dirname, '..', 'node_modules'),
+      path.join(__dirname, '..', 'app/assets/stylesheets')
+    ],
+    extensions: ['', '.js']
+  },
+
+  sassLoader: {
+    includePaths: [path.join(__dirname, '..', 'node_modules')]
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test   : /\.scss$/,
+        loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?mimetype=image/png"
+      }
+    ]
+  },
+
+  plugins: [
+    // must match config.webpack.manifest_filename
+    new StatsPlugin('manifest.json', {
+      // We only need assetsByChunkName
+      chunkModules: false,
+      source: false,
+      chunks: false,
+      modules: false,
+      assets: true
+    })]
+};
+
+if (production) {
+  config.plugins.push(
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: { warnings: false },
+      sourceMap: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production') }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  );
+} else {
+  config.devServer = {
+    port: devServerPort,
+    headers: { 'Access-Control-Allow-Origin': '*' }
+  };
+  config.output.publicPath = '//localhost:' + devServerPort + '/webpack/';
+  // Source maps
+  config.devtool = 'cheap-module-eval-source-map';
+}
+
+module.exports = config;
